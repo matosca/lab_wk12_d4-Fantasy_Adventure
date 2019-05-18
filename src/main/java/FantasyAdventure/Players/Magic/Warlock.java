2fdec941f45166cfb0ce93c80bf0c04f9af30497
marisol <mtoscanor37@gmail.com>
2019-05-18 21:07:59 +0100

    Created warlock class in Magic abs class and TDD

---
+++ src/main/java/FantasyAdventure/Players/Magic/Warlock.java
@@ -0,0 +1,12 @@
+package FantasyAdventure.Players.Magic;
+
+import FantasyAdventure.Enums.MythicalCreatures;
+import FantasyAdventure.Enums.Race;
+import FantasyAdventure.Enums.Spells;
+
+public class Warlock extends Magic{
+
+    public Warlock(String name, Race race, int wallet, Spells spell, MythicalCreatures creature) {
+        super(name, race, wallet, spell, creature);
+    }
+}
--- src/test/java/FantasyAdventure/Players/WarlockTest.java
+++ src/test/java/FantasyAdventure/Players/WarlockTest.java
@@ -3,54 +3,54 @@ package FantasyAdventure.Players;
 import FantasyAdventure.Enums.MythicalCreatures;
 import FantasyAdventure.Enums.Race;
 import FantasyAdventure.Enums.Spells;
-import FantasyAdventure.Players.Magic.Wizard;
+import FantasyAdventure.Players.Magic.Warlock;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class WizardTest {
+public class WarlockTest {
 
-    Wizard wizard;
+    Warlock warlock;
 
     @Before
     public void before(){
-        wizard = new Wizard("Potter", Race.HUMAN, 20, Spells.LIGHTNING, MythicalCreatures.HIPPOGRIFF);
+        warlock = new Warlock("Wordruk", Race.DWARF, 35, Spells.SCORCHINGRAY, MythicalCreatures.BASILISK);
     }
 
     @Test
     public void hasName(){
-        assertEquals("Potter", wizard.getName());
+        assertEquals("Wordruk", warlock.getName());
     }
 
     @Test
     public void hasRace(){
-        assertEquals(Race.HUMAN, wizard.getRace());
+        assertEquals(Race.DWARF, warlock.getRace());
     }
 
     @Test
     public void hasWallet(){
-        assertEquals(20, wizard.getWallet());
+        assertEquals(35, warlock.getWallet());
     }
 
     @Test
     public void hasSpells(){
-        assertEquals(Spells.LIGHTNING, wizard.getSpell());
+        assertEquals(Spells.SCORCHINGRAY, warlock.getSpell());
     }
 
     @Test
     public void hasSpellDamage(){
-        assertEquals(13, wizard.getDamage());
+        assertEquals(17, warlock.getDamage());
     }
 
     @Test
     public void hasAMythicalCreature(){
-        assertEquals(MythicalCreatures.HIPPOGRIFF, wizard.getCreature());
+        assertEquals(MythicalCreatures.BASILISK, warlock.getCreature());
     }
 
     @Test
     public void hasDefendPointsFromMythicalCreature(){
-        assertEquals(16, wizard.getDefendPoints()
+        assertEquals(12, warlock.getDefendPoints()
         );
     }
 
